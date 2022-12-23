2d90456008bd1981cb96707f0a876fee9a7c7399
Danil Prisiazhnyi <judas.priest999@gmail.com>
2022-12-23 12:57:21 +0400

    clean naming

--- test/version1/EmailSettingsMockClientV1_test.go
+++ test/version1/EmailSettingsMockClientV1_test.go
@@ -6,23 +6,23 @@ import (
 	"github.com/pip-services-users2/client-emailsettings-go/version1"
 )
 
-type settingsMemoryClientV1Test struct {
+type settingsMockClientV1Test struct {
 	client  *version1.EmailSettingsMockClientV1
 	fixture *EmailSettingsClientFixtureV1
 }
 
-func newsettingsMemoryClientV1Test() *settingsMemoryClientV1Test {
-	return &settingsMemoryClientV1Test{}
+func newsettingsMockClientV1Test() *settingsMockClientV1Test {
+	return &settingsMockClientV1Test{}
 }
 
-func (c *settingsMemoryClientV1Test) setup(t *testing.T) {
+func (c *settingsMockClientV1Test) setup(t *testing.T) {
 	c.client = version1.NewEmailSettingsMockClientV1()
 
 	c.fixture = NewEmailSettingsClientFixtureV1(c.client)
 }
 
 func TestMockCrudOperations(t *testing.T) {
-	c := newsettingsMemoryClientV1Test()
+	c := newsettingsMockClientV1Test()
 	c.setup(t)
 
 	c.fixture.TestCrudOperations(t)
